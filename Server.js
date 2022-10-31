bfce1bae922935a63301e34310d23bba82d00c8d
Jorden <Jordenreuter@web.de>
2022-10-31 16:10:00 +0100

    Setup frontend Reactapp
    Cleared filestructure, added first layout elements and css

--- Server.js
+++ Server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from "cors";
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
+import todo from "./models/TodoModel.js";
 
 dotenv.config();
 
@@ -14,6 +15,30 @@ mongoose.connect(process.env.MONGO_URL, {
 })
     .then(() => console.log("MongoDB connected.."))
     .catch((err) => console.error(err));
+
+app.get('/todos', async (req, res) => {
+    const todos = await todo.find();
+    res.json(todos);
+})
+
+app.post('/todo/new', (req, res) => {
+    const newTodo = new todo({
+        text: req.body.text
+    });
+    newTodo.save();
+    res.json(newTodo);
+});
+
+app.delete('/todo/delete/:id', async (req, res) => {
+    const result = await todo.findByIdAndDelete(req.params.id);
+    res.json(result);
+})
+app.put('/todo/complete/:id', async (req, res) => {
+    const completed = await todo.findById(req.params.id);
+    completed.complete = !completed.complete;
+    completed.save();
+    res.json(completed);
+});
 
 app.listen(5000, () => {
     console.log('App listening on port: 5000')
